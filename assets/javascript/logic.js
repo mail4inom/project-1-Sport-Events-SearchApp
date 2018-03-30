b8af0cc4345f08bed5c13e87e68368098470178a
Inomzhon Ibragimov <mail4inom@users.noreply.github.com>
2018-03-30 18:57:05 -0400

    test

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,9 +1,10 @@
-
 $(document).ready(function () {
     $("#search").on("click", function (event) {
+        
         $(".output").empty();
         event.preventDefault();
         var searchInput = $("#input").val().trim();
+        $("#input").val(" ");
         if (searchInput === "") {
             return;
         }
@@ -23,20 +24,29 @@ $(document).ready(function () {
                 var city = response[i].venue.extended_address;
                 var country = response[i].venue.country;
                 var time = response[i].datetime_local;
+                var place = response[i].venue.name;
                 var aPrice = response[i].stats.average_price;
                 var hPrice = response[i].stats.highest_price;
                 var lPrice = response[i].stats.lowest_price;
                 var url = response[i].url;
+                time = new Date(time);
+                aPrice = aPrice.toFixed(2);
+                hPrice = hPrice.toFixed(2);
+                lPrice = lPrice.toFixed(2);
+                img = response[i].performers[0].images.huge;
 
+                if (img === undefined) {
 
-                var img = response[i].performers[0].images.huge;
+                    img = "assets/images/image5.jpeg";
+                }
 
                 var title = $("<h2>").text(name);
                 var addresS = $("<h4>").text(address + " " + city + " " + country + ".");
-                var timE = $("<h4>").text("Time: " + time);
-                var avPrice = $("<h4>").text("Average price: " + aPrice + "$");
-                var hiPrice = $("<h4>").text("Highest price: " + hPrice + "$");
-                var loPrice = $("<h4>").text("Lowest price: " + lPrice + "$");
+                var timE = $("<h4>").text(time);
+                var placE = $("<h4>").text(place);
+                var avPrice = $("<h4>").text("Average price: " + "$" + aPrice);
+                var hiPrice = $("<h4>").text("Highest price: " + "$" + hPrice);
+                var loPrice = $("<h4>").text("Lowest price: " + "$" + lPrice);
                 var tickets = $("<a>");
                 tickets.attr("href", url);
                 tickets.text("Click here to purchase a ticket for this event!");
@@ -45,10 +55,12 @@ $(document).ready(function () {
 
                 image.attr("src", img);
 
-                newDiv.append(title, addresS, timE, avPrice, hiPrice, loPrice, tickets, image);
+                newDiv.append(title, placE, addresS, timE, avPrice, hiPrice, loPrice, tickets, image);
                 $(".output").append(newDiv);
+
             }
         });
 
     });
+
 });
