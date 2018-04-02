9877e8286c12ce076ac7d64572441232102439b6
Inomzhon Ibragimov <mail4inom@users.noreply.github.com>
2018-04-02 04:49:17 -0400

    team page added

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,6 +1,10 @@
 $(document).ready(function () {
+    $(".title").hide();
     $("#search").on("click", function (event) {
-        
+        $(".black").css("background-color", "white")
+        $(".hide").hide();
+        $(".title").show();
+        $(".output").show();
         $(".output").empty();
         event.preventDefault();
         var searchInput = $("#input").val().trim();
@@ -8,7 +12,7 @@ $(document).ready(function () {
         if (searchInput === "") {
             return;
         }
-        var queryURL = "https://api.seatgeek.com/2/events?geoip=true&performers.slug&q=" + searchInput + "&client_id=MTEwMzI0MzR8MTUyMjMzNDkxMy42MQ";
+        var queryURL = "https://api.seatgeek.com/2/events?geoip=true&&range=100mi&q=" + searchInput + "&client_id=MTEwMzI0MzR8MTUyMjMzNDkxMy42MQ";
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -17,46 +21,66 @@ $(document).ready(function () {
             var response = response.events;
 
             for (var i = 0; i < response.length; i++) {
-                var newDiv = $("<div>");
-                newDiv.addClass("col-md-12");
+
+                var divRow = $("<div>").addClass("row  jumbo");
+                var newDiv = $("<div>").addClass("col-md-6 col-xs-6");
+                var newDivImg = $("<div>").addClass("col-md-6 col-xs-6");
+
+
                 var name = response[i].title;
+
                 var address = response[i].venue.address;
                 var city = response[i].venue.extended_address;
                 var country = response[i].venue.country;
                 var time = response[i].datetime_local;
                 var place = response[i].venue.name;
+
                 var aPrice = response[i].stats.average_price;
                 var hPrice = response[i].stats.highest_price;
                 var lPrice = response[i].stats.lowest_price;
                 var url = response[i].url;
+
                 time = new Date(time);
+
                 aPrice = aPrice.toFixed(2);
                 hPrice = hPrice.toFixed(2);
                 lPrice = lPrice.toFixed(2);
-                img = response[i].performers[0].images.huge;
+
+                var img = response[i].performers[0].images.huge;
 
                 if (img === undefined) {
-
                     img = "assets/images/image5.jpeg";
                 }
 
-                var title = $("<h2>").text(name);
-                var addresS = $("<h4>").text(address + " " + city + " " + country + ".");
-                var timE = $("<h4>").text(time);
-                var placE = $("<h4>").text(place);
-                var avPrice = $("<h4>").text("Average price: " + "$" + aPrice);
-                var hiPrice = $("<h4>").text("Highest price: " + "$" + hPrice);
-                var loPrice = $("<h4>").text("Lowest price: " + "$" + lPrice);
-                var tickets = $("<a>");
-                tickets.attr("href", url);
-                tickets.text("Click here to purchase a ticket for this event!");
+                var title = $("<p>").text(name);
+                title.addClass("eventName")
+                var addresS = $("<p>").text(address + " " + city + " " + country + ".");
+                var timE = $("<p>").text(time);
+                var placE = $("<p>").text(place);
+                placE.addClass("place");
+
+                var avPrice = $("<p>").text("Average price: " + "$" + aPrice);
+                var hiPrice = $("<p>").text("Highest price: " + "$" + hPrice);
+                var loPrice = $("<p>").text("Lowest price: " + "$" + lPrice);
+
+                var tickets = $("<p>");
+                var showEvents = $("<a>");
+                showEvents.addClass("btn btn-primary button")
+                showEvents.attr("href", url);
+                showEvents.text("Buy Ticket");
+
+                tickets.append(showEvents);
+
+                tickets.attr("id", "showEvents");
 
                 var image = $("<img>");
-
+                image.addClass("apiImage")
                 image.attr("src", img);
 
-                newDiv.append(title, placE, addresS, timE, avPrice, hiPrice, loPrice, tickets, image);
-                $(".output").append(newDiv);
+                newDiv.append(placE, addresS, timE, avPrice, hiPrice, loPrice, tickets);
+                newDivImg.append(title, image);
+                divRow.append(newDivImg, newDiv);
+                $(".output").append(divRow);
 
             }
         });
